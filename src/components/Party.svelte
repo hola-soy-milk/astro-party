431c18546c171f95cd050afa30e98e931a2f3c2b
Ramón Huidobro <hola@ramonh.dev>
2023-10-21 13:41:47 +0200

    Integrate Svelte PartyKit client

---
+++ src/components/Party.svelte
@@ -0,0 +1,23 @@
+<script>
+  // client
+  import PartySocket from "partysocket";
+
+  let number = 0;
+
+  // connect to our server
+  const partySocket = new PartySocket({
+    host: "localhost:1999",
+    room: "my-room",
+  });
+
+  // send a message to the server
+  partySocket.send("Hello everyone");
+
+  // print each incoming message from the server to console
+  partySocket.addEventListener("message", (e) => {
+    number = e.data;
+  });
+</script>
+
+<!-- {src} is short for src={src} -->
+<h1>{number}</h1>
